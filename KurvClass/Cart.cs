4ab6bc406b12d250333803e58e5d68567649a196
bbr <bbr@rts.dk>
2016-08-09 13:57:24 +0200

    Added cart class to project.

---
+++ KurvClass/Cart.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Optiguy
+{
+    public class Cart
+    {
+        private List<CartProduct> items; //This is all the items in the cart
+
+        public Cart()
+        {
+            this.items = GrabCart();
+        }
+
+        public List<CartProduct> Items { get { return this.items; } }
+
+        private List<CartProduct> GrabCart()
+        {
+            List<CartProduct> cart = new List<CartProduct>();
+            
+            if (HttpContext.Current.Session["Cart"] == null)
+            {
+                HttpContext.Current.Session.Add("Cart", cart);
+            }
+
+            cart = HttpContext.Current.Session["Cart"] as List<CartProduct>;
+            return cart;
+        }
+
+        private void AddToCart(int id, string name, decimal price, int amount)
+        {
+            bool newProduct = true;
+
+            foreach (CartProduct product in this.items)
+            {
+                if (product.Id == id)
+                {
+                    newProduct = false;
+                    product.Amount += amount;
+                }
+            }
+
+            if (newProduct)
+            {
+                this.items.Add(new CartProduct(id,name,price,amount));
+            }
+        }
+    }
+}
--- KurvClass/CartProduct.cs
+++ KurvClass/CartProduct.cs
@@ -12,7 +12,6 @@ namespace Optiguy
         private string name;
         private decimal price;
         private int amount;
-        private decimal totalPrice;
         #endregion
 
         #region properties
@@ -20,17 +19,15 @@ namespace Optiguy
         public string Name { get { return this.name; } set { this.name = value; } }
         public decimal Price { get { return this.price; } set { this.price = value; } }
         public int Amount { get { return this.amount; } set { this.amount = value; } }
-        public decimal TotalPrice { get { return this.totalPrice; } set { this.totalPrice = value; } }
+        public decimal TotalPrice { get { return this.price * this.amount; } }
         #endregion
 
         #region constructors
-        public CartProduct() { }
         public CartProduct(int id, string name, decimal price, int amount) {
             this.id = id;
             this.name = name;
             this.price = price;
             this.amount = amount;
-            this.totalPrice = this.price * this.amount;
         }
 
         #endregion
