b9ca0246bf60efb7362fa69581be208f598dc87e
bbr <bbr@rts.dk>
2016-08-11 10:41:45 +0200

    Started on order system

---
+++ Ordre/Order.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Optiguy
+{
+
+    public class Order
+    {
+        private int orderId;
+        private int userId; 
+        private DateTime createdDate;
+        private List<CartProduct> products;
+        
+        public int OrderId { get { return this.orderId; } }
+        public int UserId { get { return this.userId; } set { this.userId = value; } }
+        public DateTime CreatedDate { get { return this.createdDate; } set { this.createdDate = value; } }
+        public List<CartProduct> Products { get { return this.products; } }
+
+        public Order(int userId) {
+            this.userId = userId;
+            this.createdDate = DateTime.Now;
+        }
+
+        public void addProduct(CartProduct item){
+            this.products.Add(item);
+        }
+
+        public void saveOrder() {
+            //Opret en ordre i databasen - Returner id'et
+            //Brug id'et fra ordren til at oprette alle ordre linier med.
+        }
+
+    }
+}
---
+++ Ordre/checkout.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Optiguy;
+
+namespace Optiguy
+{
+    public partial class checkout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
